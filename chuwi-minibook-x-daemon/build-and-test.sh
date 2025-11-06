ea5064770e9dede538fd80f14f784585e94a9e3b
Armando DiCianno <armando@noonshy.com>
2025-11-06 15:14:26 -0500

    Prune old module and daemon.

--- chuwi-minibook-x-daemon/build-and-test.sh
+++
@@ -1,3 +0,0 @@
-cc -O2 -o chuwi-minibook-x-tablet-mode chuwi-minibook-x-tablet-mode.c -lm && \
-    sudo cp chuwi-minibook-x-tablet-mode /usr/local/sbin/ && \
-    sudo chuwi-minibook-x-tablet-mode iio:device0 iio:device1 100
